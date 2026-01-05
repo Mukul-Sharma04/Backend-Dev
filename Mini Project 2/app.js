0ff5684d5f78f99d0a6c5324c4335a256181cb71
Mukul Sharma <sharmamukul1204@gmail.com>
2026-01-05 15:10:41 +0530

    Add feature of create posts in mini project

--- Mini Project 2/app.js
+++ Mini Project 2/app.js
@@ -50,7 +50,7 @@ app.post("/login", async (req, res) => {
     if (result) {
       let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
       res.cookie("token", token);
-      res.status(200).send(" you can login");
+      res.status(200).redirect("/profile");
     } else res.redirect("/login");
   });
 });
@@ -60,13 +60,27 @@ app.get("/logout", (req, res) => {
   res.redirect("/login");
 });
 
-app.get("/profile",isLoggedIn, (req, res)=>{
-    console.log(req.user)
-    res.redirect("/login")
+app.get("/profile",isLoggedIn, async (req, res)=>{
+  let user = await userModel.findOne({email: req.user.email}).populate("posts");
+  res.render("profile", {user})
 })
 
+app.post("/post", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  let {content} = req.body;
+
+  let post = await postModel.create({
+    user: user._id,
+    content
+  });
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile")
+});
+
 function isLoggedIn(req, res, next) {
-  if (req.cookies.token === "") res.send("You must be logged in");
+  if (req.cookies.token === "") res.redirect("/login");
   else {
     let data = jwt.verify(req.cookies.token, "shhhh");
     req.user = data;
